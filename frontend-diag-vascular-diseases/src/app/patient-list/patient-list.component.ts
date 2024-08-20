d8256688282da42275b7b9d68cd579a82d823fca
mboup <mboup124@gmail.com>
2024-08-20 15:26:15 +0200

    search for a patient by first name, last name or Medical Record Number

--- frontend-diag-vascular-diseases/src/app/patient-list/patient-list.component.ts
+++ frontend-diag-vascular-diseases/src/app/patient-list/patient-list.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Patient } from '../models/patient.model';
 import { PatientService } from '../patient.service';
 import { Router } from '@angular/router';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-patient-list',
@@ -11,19 +12,57 @@ import { Router } from '@angular/router';
 export class PatientListComponent {
 
   patients: Patient[] = [];
+  filteredPatients: Patient[] = [];
   displayedColumns: string[] = ['name', 'heartRate', 'temperature', 'bloodPressure', 'oxygenSaturation', 'actions'];
+
+  searchTermControl = new FormControl('');
+  sortByControl = new FormControl('name');
+
 
   constructor(private patientService: PatientService, private router: Router) { }
 
   ngOnInit(): void {
     this.loadPatients();
     setInterval(() => this.loadPatients(), 60000);
+
+    this.searchTermControl.valueChanges.subscribe(() => this.filterPatients());
+    this.sortByControl.valueChanges.subscribe(() => this.sortPatients());
+
   }
 
   loadPatients(): void {
-    this.patientService.getPatients().subscribe(data => this.patients = data);
-    // console.log("this.patients : ", this.patients)
+    this.patientService.getPatients().subscribe(data => {
+      this.patients = data;
+      this.filteredPatients = [...this.patients];
+      this.filterPatients();
+    });
   }
+
+  filterPatients(): void {
+    const searchTerm = this.searchTermControl.value?.trim().toLowerCase() || '';
+    this.filteredPatients = this.patients.filter(patient =>
+      patient.firstName.toLowerCase().includes(searchTerm) ||
+      patient.lastName.toLowerCase().includes(searchTerm) ||
+      patient.medicalRecordNumber.toLowerCase().includes(searchTerm)
+    );
+    this.sortPatients();
+  }
+
+  sortPatients(): void {
+    const sortBy = this.sortByControl.value;
+    this.filteredPatients.sort((a, b) => {
+      if (sortBy === 'name') {
+        return a.lastName.localeCompare(b.lastName);
+      } else if (sortBy === 'firstName') {
+        return a.firstName.localeCompare(b.firstName);
+      }
+      else if (sortBy === 'medicalRecordNumber') {
+        return a.medicalRecordNumber.localeCompare(b.medicalRecordNumber);
+      }
+      return 0;
+    });
+  }
+
 
   viewPatientDetails(patient: Patient): void {
     this.router.navigate(['/patient-details', patient.id]);
